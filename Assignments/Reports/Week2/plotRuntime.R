6dd45b3166f510fa70ca10842fa75e108b0c5333
adamingwersen <adamlinnemann@gmail.com>
2016-09-20 09:28:30 +0200

    added file

--- Assignments/Reports/Week2/plotRuntime.R
+++ Assignments/Reports/Week2/plotRuntime.R
@@ -1,7 +1,16 @@
 fun1 <- function(x) x^2
 fun2 <- function(x) (x*(x-1)^2)/2
+fun3 <- function(x) x*log2(x)
 x<-seq(0,4*pi,0.01)
-matplot(x, cbind(fun1(x), fun2(x)),
-        type = "l", col = c("blue", "red"), 
-        ylab = "Runtime", xlab = "n")
-legend("bottomright", inset = .05, legend = c("n²", "(n(n-1)²)/2"), pch = 1, col = c(2,4), horiz = T)
+
+data <- cbind(fun1(x), fun2(x), fun3(x))
+
+matplot(x, data, type = 'l', 
+	col = c("#ff0000", "#00008b", "#31720a"),
+	ylab = "Køretid", xlab = "n")
+legend("topright", inset = .05, 
+	legend = c("(n-1)²", "(n(n-1)²)/2", "n(log(n))"),
+    pch = 1, 
+    col = c("#ff0000", "#00008b", "#31720a"), horiz = TRUE)
+title("Sammenligning af køretider")
+
